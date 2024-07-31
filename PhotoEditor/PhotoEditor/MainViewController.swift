16ffe48781fd8c904d3bf39d002c0ac067695273
mkotyak <kotyak.maria@gmail.com>
2024-07-31 10:21:41 +0300

    Created tab bar screen

---
+++ PhotoEditor/PhotoEditor/MainViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    private func configureUI() {
+        view.backgroundColor = .red
+    }
+}
---
+++ PhotoEditor/PhotoEditor/SettingsViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class SettingsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    private func configureUI() {
+        view.backgroundColor = .green
+    }
+}
