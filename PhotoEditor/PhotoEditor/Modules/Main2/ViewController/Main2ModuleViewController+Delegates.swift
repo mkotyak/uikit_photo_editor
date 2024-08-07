c2e724f9e2c3c963ff59772a3107bf39c8c0ed3a
mkotyak <kotyak.maria@gmail.com>
2024-08-07 13:13:28 +0300

    Implemented photo editor view

--- PhotoEditor/PhotoEditor/Modules/Main2/ViewController/Main2ModuleViewController+Delegates.swift
+++ PhotoEditor/PhotoEditor/Modules/Main2/ViewController/Main2ModuleViewController+Delegates.swift
@@ -1,1 +1,43 @@
-import Foundation
+import UIKit
+
+// MARK: - UINavigationControllerDelegate
+
+extension Main2ModuleViewController: UINavigationControllerDelegate {}
+
+// MARK: - UIImagePickerControllerDelegate
+
+extension Main2ModuleViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
+    ) {
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            return
+        }
+
+        viewModel.viewDidSelectImage(selectedImage)
+        addButton.removeFromSuperview()
+        setupPhotoEditorView()
+        dismiss(animated: true)
+    }
+
+    private func resetImageView() {
+//        imageView?.removeFromSuperview()
+//        imageView = nil
+    }
+
+    private func resetSegmentedControl() {
+//        segmentedControl.selectedSegmentIndex = 0
+    }
+}
+
+// MARK: - UIGestureRecognizerDelegate
+
+extension Main2ModuleViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+    ) -> Bool {
+        return true
+    }
+}
--- PhotoEditor/PhotoEditor/Modules/Main2/ViewController/Main2ModuleViewController.swift
+++ PhotoEditor/PhotoEditor/Modules/Main2/ViewController/Main2ModuleViewController.swift
@@ -2,10 +2,34 @@ import Combine
 import UIKit
 
 class Main2ModuleViewController: UIViewController {
+    let viewModel: Main2ModuleViewModel = .init()
+    var cancellables: Set<AnyCancellable> = .init()
+    let imageView: UIImageView = .init()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+
         setupAddButton()
+        setupBinding()
+    }
+
+    func setupPhotoEditorView() {
+        view.addSubview(photoEditorView)
+        photoEditorView.translatesAutoresizingMaskIntoConstraints = false
+        photoEditorView.center = .init(x: view.center.x, y: view.center.y)
+    }
+
+    private func setupBinding() {
+        viewModel.selectedImage
+            .sink { [weak self] newImage in
+                guard let self else {
+                    return
+                }
+
+                updateImageView(with: newImage)
+            }
+            .store(in: &cancellables)
     }
 
     private func setupAddButton() {
@@ -17,14 +41,27 @@ class Main2ModuleViewController: UIViewController {
         ])
     }
 
+    private func updateImageView(with newImage: UIImage?) {
+        imageView.image = newImage
+    }
+
     // MARK: - UI Elements
 
     // addButton
-    private lazy var addButton: UIButton = {
+
+    lazy var addButton: UIButton = {
         let button = UIButton(type: .custom)
         button.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
-        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.setImage(
+            UIImage(systemName: "plus"),
+            for: .normal
+        )
         button.imageView?.contentMode = .scaleAspectFit
+        button.addTarget(
+            self,
+            action: #selector(addButtonTapped),
+            for: .touchUpInside
+        )
 
         if let imageView = button.imageView {
             imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -39,7 +76,50 @@ class Main2ModuleViewController: UIViewController {
         return button
     }()
 
-    @objc private func plusButtonTapped() {}
+    @objc private func addButtonTapped() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
 
-    // another element
+        present(
+            imagePickerController,
+            animated: true
+        )
+    }
+
+    // photoEditorView
+
+    private lazy var photoEditorView: UIView = {
+        let photoEditorView = UIView(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: 350,
+            height: 350
+        ))
+        photoEditorView.layer.borderWidth = 4
+        photoEditorView.layer.borderColor = UIColor.yellow.cgColor
+
+        photoEditorView.addSubview(imageView)
+        photoEditorView.clipsToBounds = true
+
+        imageView.frame = photoEditorView.bounds
+        imageView.isUserInteractionEnabled = true
+        addGestureRecognizers(to: imageView)
+
+        return photoEditorView
+    }()
+
+    private func addGestureRecognizers(to view: UIView) {
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch))
+        let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(handleRotation))
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+
+        pinchGesture.delegate = self
+        rotationGesture.delegate = self
+        panGesture.delegate = self
+
+        view.addGestureRecognizer(pinchGesture)
+        view.addGestureRecognizer(rotationGesture)
+        view.addGestureRecognizer(panGesture)
+    }
 }
